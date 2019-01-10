91e4fd2249e66fd03e0f4bde003e315769569c2c
zevs00807 <xmelayx@gmail.com>
2019-01-10 17:03:23 +0200

    Update entrypoint.sh

--- db_cluster/entrypoint.sh
+++ db_cluster/entrypoint.sh
@@ -111,9 +111,9 @@ export PATRONI_POSTGRESQL_PGPASS="$HOME/.pgpass"
 
 cat > /patroni.yml <<__EOF__
 etcd :
-   hosts :
-    - etcd1:2379 
-    - etcd2:2379 
+   host :
+    - etcd:2379 
+#    - etcd2:2379 
 
 bootstrap:
   dcs:
