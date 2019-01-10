44bcfb32638910ffba51163518585c56937d2e0a
root <root@swarm-1.europe-west4-a.c.top-suprstate-220509.internal>
2019-01-10 09:41:33 +0000

    add

--- db_cluster/entrypoint.sh
+++ db_cluster/entrypoint.sh
@@ -103,6 +103,11 @@ export PATRONI_SUPERUSER_PASSWORD="${PATRONI_SUPERUSER_PASSWORD:-postgres}"
 export PATRONI_POSTGRESQL_PGPASS="$HOME/.pgpass"
 
 cat > /patroni.yml <<__EOF__
+etcd :
+   hosts :
+    - etcd1:2379 
+    - etcd2:2379 
+
 bootstrap:
   dcs:
     postgresql:
